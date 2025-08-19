3dd820a8be39c909e390025f2a72101e80cedda4
André Filipe <br.dre.developer@gmail.com>
2025-08-19 11:01:52 -0300

    Pine.java has been updated

--- Pine/Pine.java
+++ Pine/Pine.java
@@ -1,9 +1,9 @@
 public class Pine {
 	public static void main(String args[]) {
 		String c = "*";
-		int numberLines = Integer.parseInt(args[0]);
+		int numberOfLines = Integer.parseInt(args[0]);
 
-		for(int i = numberLines; i > 0; i--) {
+		for(int i = numberOfLines; i > 0; i--) {
 			String s = "";
 			for(int j = 0; j < i - 1; j++) {
 				s += " ";
